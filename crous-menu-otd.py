3b8be271f1003d8f99de1752d70761139de4b21b
Adrien Couetoux <adrien.couetoux@etu.u-bordeaux.fr>
2026-06-09 22:08:20 +0200

    clean: debug + type annotations

--- crous-menu-otd.py
+++ crous-menu-otd.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 from datetime import datetime
-from bs4 import BeautifulSoup
+from bs4 import (BeautifulSoup, Tag)
 import requests
+import pyperclip
 
 days: dict[str, str] = {
   'Monday': 'lundi',
@@ -28,51 +29,41 @@ months: dict[str, str] = {
   'December': 'décembre'
 }
 
-def get_today_str():
+def get_today_str() -> str:
   split = datetime.today().strftime('%A %-d %B %Y').split(' ')
   split[0] = days[split[0]] # translate day
   split[2] = months[split[2]] # translate month
   return ' '.join(split)
 
-def parse(url: str):
+def parse(url: str) -> Tag | None:
   response = requests.get(url)
-  soup = BeautifulSoup(response.content, 'html.parser')
-  all_menus = soup.find_all('div', { 'class': 'menu'})
+  soup: BeautifulSoup = BeautifulSoup(response.content, 'html.parser')
+  all_menus: list[Tag] = soup.find_all('div', { 'class': 'menu'})
   today_str = get_today_str()
+  # menus day by day
   for menu in all_menus:
-    menu_date = menu.find('time').get_text()
+    # check the date
+    menu_date: str = menu.find('time').get_text()
     if today_str in menu_date:
-      print("FOUND THE MENU!!")
       return menu.find('ul', { 'class': 'meal_foodies'})
-    
-def print_menu(menu_element):
+  return None
+
+def format_menu(menu_element: Tag) -> str:
+  menu_str: str = ""
   for section in menu_element.children:
-    section_str = section.encode().decode()
-    section_str = section_str[4:] # remove <li> prefix
-    section_str = section_str.replace('</li></ul></li>', '\n') # section separator
-    section_str = section_str.replace('<ul>', '\n') # title <> first entry separator
-    section_str = section_str.replace('<li>', '\t') # sub-entry indent (actual food)
-    section_str = section_str.replace('</li>', '\n') # sub-entries separator
-    print(section_str)
+    menu_str += section.encode().decode()
+    menu_str = menu_str[4:] # remove <li> prefix
+    menu_str = menu_str.replace('</li></ul></li>', '\n') # section separator
+    menu_str = menu_str.replace('<ul>', '\n') # title <> first entry separator
+    menu_str = menu_str.replace('<li>', '\t') # sub-entry indent (actual food)
+    menu_str = menu_str.replace('</li>', '\n') # sub-entries separator
+  return menu_str
 
-# DEBUG 
-def print_menu_str(menu_str):
-  for section_str in menu_str:
-    section_str = section_str[4:]
-    section_str = section_str.replace('</li></ul></li>', '\n')
-    section_str = section_str.replace('<ul>', '\n')
-    section_str = section_str.replace('<li>', '\t')
-    section_str = section_str.replace('</li>', '\n')
-    print(section_str)
+lamazone: str = 'https://www.crous-bordeaux.fr/restaurant/resto-u-n1-ferme-pour-travaux-2/'
+today_menu: Tag | None = parse(lamazone)
+if today_menu is None:
+  exit("uhoh couldn't find the menu - check it's a weekday, or maybe you got firewalled")
+menu_str: str = format_menu(today_menu)
 
-lamazone = 'https://www.crous-bordeaux.fr/restaurant/resto-u-n1-ferme-pour-travaux-2/'
-today_menu = parse(lamazone)
-# DEBUG
-# today_menu = [
-#   "<li>ENTREES<ul><li>menu non communiqué</li></ul></li>",
-#   "<li>PLAT DU JOUR - STAND 1<ul><li>Sauté de dinde à la vanille</li><li>Boulgour aux amandes</li><li>Poêlée de poivrons</li></ul></li>",
-#   "<li>PLAT DU JOUR - STAND 3<ul><li>Sauté de dinde à la vanille</li><li>Boulgour aux amandes</li><li>Poêlée de poivrons  Ou</li><li>Alternative sans viande :</li><li>Flan courgettes parmesan</li></ul></li>",
-#   "<li>DESSERT<ul><li>Dessert lacté ou Fruit ou Compote BIO</li><li>Fromage blanc BIO</li><li>Tarte noix de coco</li><li>( Dessert supplément )</li><li>Pain BIO</li></ul></li>"
-# ]
-# print_menu_str(today_menu)
-print_menu(today_menu)
+pyperclip.copy(menu_str) # copy to clipboard
+print(menu_str) # print for feedback
